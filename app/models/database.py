53ca71f3e662954e69dadb30b1a288a1a41e5638
Gabriel Lacorte <124625337+Gabriel-Lacorte@users.noreply.github.com>
2023-07-25 16:42:49 -0300

    Update database.py

--- app/models/database.py
+++ app/models/database.py
@@ -1,7 +1,12 @@
 #from sqlite3 import connect
 from mysql.connector import connect
 
-db = connect()
+db = connect(
+    host="",
+    user="",
+    password="",
+    database=""
+)
 cursor = db.cursor()
 
 def obter_todos_posts():
@@ -9,35 +14,35 @@ def obter_todos_posts():
     return cursor.fetchall()
 
 def obter_post_por_id(post_id):
-    cursor.execute('SELECT * FROM Posts WHERE id = ?', (post_id,))
+    cursor.execute('SELECT * FROM Posts WHERE id = %s', (post_id,))
     return cursor.fetchall()
 
 def inserir_post(title, content, author):
-    sql = "INSERT INTO Posts (title, content, author) VALUES (?, ?, ?)"
+    sql = "INSERT INTO Posts (title, content, author) VALUES (%s, %s, %s)"
     valores = (title, content, author)
     cursor.execute(sql, valores)
     db.commit()
 
 def excluir_post_por_id(post_id):
-    cursor.execute('DELETE FROM Posts WHERE id = ?', (post_id,))
+    cursor.execute('DELETE FROM Posts WHERE id = %s', (post_id,))
     db.commit()
 
 def obter_comentarios_por_post_id(post_id):
-    cursor.execute('SELECT content, author FROM Comments WHERE postid = ?', (post_id,))
+    cursor.execute('SELECT content, author FROM Comments WHERE postid = %s', (post_id,))
     return cursor.fetchall()
 
 def inserir_comentario(content, author, post_id):
-    sql = 'INSERT INTO Comments (content, author, postid) VALUES (?, ?, ?)'
+    sql = 'INSERT INTO Comments (content, author, postid) VALUES (%s, %s, %s)'
     valores = (content, author, post_id)
     cursor.execute(sql, valores)
     db.commit()
 
 def obter_usuario_por_nome(usuario):
-    cursor.execute('SELECT id, user, password FROM Users WHERE user = ?', (usuario,))
+    cursor.execute('SELECT id, user, password FROM Users WHERE user = %s', (usuario,))
     return cursor.fetchone()
 
 def inserir_usuario(usuario, senha_hash):
-    sql = "INSERT INTO Users (user, password) VALUES (?, ?)"
+    sql = "INSERT INTO Users (user, password) VALUES (%s, %s)"
     valores = (usuario, senha_hash)
     cursor.execute(sql, valores)
     db.commit()
